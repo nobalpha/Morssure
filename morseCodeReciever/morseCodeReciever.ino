630820a61163495e51dc42b314c6512c057e027f
nobalpha <roni.kolukisayan@nds.k12.tr>
2021-12-31 15:51:36 +0300

    new algorithm, improved control flow, need to use native char btw

--- morseCodeReciever/morseCodeReciever.ino
+++ morseCodeReciever/morseCodeReciever.ino
@@ -1,30 +1,71 @@
 #include <SoftwareSerial.h>
-#include <String.h>
+//#include <String.h>
 
 SoftwareSerial BTSerial(10, 11);
 
 const int vibratorPin = A0;
 
-const float timespeed = 1.0;
 const int millisecond = 1000;
-const float wpm = 5.0 / 6.0; // 1 wpm = "PARIS " per minute = 50 units per minute
-const float timeunit = 6.0 / 5.0 * wpm * millisecond;
+const float wpm = 20.0; // 1 wpm = "PARIS " per minute => 50 units per minute
+const float ups = 5.0 / 6.0 * wpm; // 5 units per 6 seconds
+const float timeunit = millisecond / ups; // milliseconds per unit
 
 /* Morse Code Timing Table */
 // . 1 tu
 // - 3 tu
-// SS 1 tu
-// SL 3 tu
-// SW 7 tu
+// US 1 tu (Unit Space)
+// CS 3 tu (Character Space)
+// WS 7 tu (Word Space)
 
 const int timingtable[5] = {1, 3, 1, 3, 7};
 
-char *morseCode, *latinCode;
+char morseCode[] = "", latinCode[] = "";
 
 const char* morseAlphabet[26] = {
   ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--",
   "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
 };
+
+const char CUSTOM_ENCODING_TABLE[5] = {'.', '-', 'U', 'W', 'C'};
+
+void dataParser(String encoded) {
+  const int encoded_length = encoded.length();
+  Serial.println(encoded_length);
+  String decoded = encoded;
+  for (int i = 0; i < encoded_length; i++) {
+    decoded.setCharAt(i, customDecoder(encoded[i]));
+    Serial.println(decoded);
+  }
+  
+  decoded.replace("U", "");
+  
+  // Delay due to the char space (data is cut by char space, so we need to simulate it...)
+
+}
+
+String split(char seperator, String string) {
+  
+}
+
+int count(char chr, char string[]) {
+  int counter = 0;
+  for (int i = 0; i < strlen(string); i++) {
+    if (string[i] == chr) counter++;
+  }
+  return counter;
+}
+
+char* str_remove(char chr, char string[]) {
+  const int str_len = strlen(string);
+  char removed[str_len - count(chr, string)];
+  for (int i = 0, j = 0; i < str_len; i++) {
+    if (string[i] == chr) {
+      removed[j] = customDecoder(string[i]);
+      j++;
+    }
+  }
+   return removed;
+}
 
 char morseToLatin(char* morseCode) {
   for (int i = 0; i < sizeof(morseAlphabet); i++) {
@@ -35,19 +76,26 @@ char morseToLatin(char* morseCode) {
   }
 }
 
+char customDecoder(char code) {
+  return CUSTOM_ENCODING_TABLE[code - '0' - 1];
+}
+
 void morseToPulse(char* morseCode) {
-  for (int i = 0; i < sizeof(morseCode); i++) {
+  for (int i = 0; i < strlen(morseCode); i++) {
+    Serial.println(morseCode[i]);
     if (morseCode[i] == '.') {
-      Serial.println(morseCode[i]);
       digitalWrite(vibratorPin, 1);
-      Serial.println(timingtable[0] * timeunit);
       delay(timingtable[0] * timeunit);
       digitalWrite(vibratorPin, 0);
     } else if (morseCode[i] == '-') {
       digitalWrite(vibratorPin, 1);
       delay(timingtable[1] * timeunit);
       digitalWrite(vibratorPin, 0);
+    } else {
+      digitalWrite(vibratorPin, 0);
+      delay(timingtable[3] * timeunit);
     }
+    delay(timingtable[2] * timeunit);
   }
 }
 
@@ -58,20 +106,23 @@ void setup()
   pinMode(vibratorPin, OUTPUT);
 
   Serial.begin(9600);
-  //morseToLatin(".-");
-  morseToPulse(".-");
-  BTSerial.begin(38400);  // HC-05 default speed in AT command more
+  Serial.print("Time unit is set:");
+  Serial.println(timeunit);
+  // morseToPulse(".-.. --- .-. . --   .. .--. ... ..- --   -.. --- .-.. --- .-.   ... .. -   .- -- . - --..--   -.-. --- -. ... . -.-. - . - ..- . .-.   .- -.. .. .--. .. ... -.-. .. -. --.   . .-.. .. - .-.-.-   ..- -   --- -.. .. --- .-.-.-   -. .- --   ... . -..   . ... - .-.-.-   -. .- --   .-  ");
+  dataParser("13141");
+  // BTSerial.begin(38400);  // HC-05 default speed in AT command more
 }
 
 void loop()
 {
-  // Keep reading from HC-05 and send to Arduino Serial Monitor
-  if (BTSerial.available() > 0) {
+  /*
+    // Keep reading from HC-05 and send to Arduino Serial Monitor
+    if (BTSerial.available() > 0) {
     char buffer = BTSerial.read();
     Serial.println();
     Serial.println(morseCode);
     morseToPulse(morseCode);
-  }
-
+    }
+  */
 
 }
--- morseCodeTransciever/morseCodeTransciever.ino
+++ morseCodeTransciever/morseCodeTransciever.ino
@@ -1,4 +1,4 @@
-//TODO: 
+//TODO:
 // Refactoring
 // Commenting
 // Debugging
@@ -10,145 +10,142 @@ SoftwareSerial BTSerial(10, 11);
 
 // const int buzzerPin = 3;
 // const int ledPin = 9;
-const int vibratorPin = 6;     
+const int vibratorPin = 6;
 const int sensorPin = A0;
-const int activatorPin = 2; 
+const int activatorPin = 2;
 const int threshold = 500.0;
-const float timespeed = 1.0;
 const int millisecond = 1000;
-const float wpm = 1.0; // 1 wpm = "PARIS " per minute = 50 units per minute
-const float timeunit = 6.0 / 5.0 / wpm * millisecond;
-const float padding = 1.0 / 4.0 * timeunit;
+const float wpm = 1.0; // 1 wpm = "PARIS " per minute => 50 units per minute
+const float ups = 5.0 / 6.0 * wpm; // 5 units per 6 seconds
+const float timeunit = millisecond / ups; // milliseconds per unit
+const float padding = 1.0 / 6.0 * timeunit;
+const float filter_limit = 100.0;
 
 /* Morse Code Timing Table */
 // . 1 tu
 // - 3 tu
-// SS 1 tu
-// SL 3 tu
-// SW 7 tu
+// US 1 tu (Unit Space)
+// CS 3 tu (Character Space)
+// WS 7 tu (Word Space)
 
 const int timingtable[5] = {1, 3, 1, 3, 7};
+const char CUSTOM_ENCODING_TABLE[5] = {'.', '-', 'U', 'W', 'C'};
 
 float value, delta_time;
 float initial_time, final_time = millis();
+bool started = false;
 
-String morseCode;
+String morseCode = "";
 
-void morseToPulse(char* morseCode) {
-    for (int i = 0; i < sizeof(morseCode); i++) {
-      if (morseCode[i] == '.') {
-        Serial.println(morseCode[i]);
-        digitalWrite(vibratorPin, 1);
-        delay(timingtable[0] * timeunit);  
-        digitalWrite(vibratorPin, 0);
-        delay(timingtable[0] * timeunit);  
-      } else if (morseCode[i] == '-') {
-        digitalWrite(vibratorPin, 1);
-        delay(timingtable[1] * timeunit);  
-        digitalWrite(vibratorPin, 0);  
-        delay(timingtable[0] * timeunit);  
-      }
-    }
-}
-
-void timeunitToMorse(int mode, float delta_time) {
-
-  if (mode == 1) {
-      if ((timeunit * timingtable[1]) - padding <= delta_time) {
-        Serial.println('-');
-        morseCode += '-';
-      } else if (50 <= delta_time) { 
-        morseCode += '.';
-        Serial.println('.');
-
-      }
-  } else if (mode == 0) {
-      if ((timeunit * timingtable[3]) - padding <= delta_time) {
-        // Serial.println("Inter-character Space");
-        if (morseCode.length()) {
-          // Send bluetooth
-          Serial.println("[OK] Starting bluetooth transaction...");
-            Serial.println(morseCode);
-            int size = morseCode.length();
-            char copy[size];
-            strcpy(copy, morseCode.c_str());
-            Serial.println(copy); 
-            char str[80];
-            sprintf(str, "%s", morseCode);
-            puts(str);
-            BTSerial.write(copy, size);
-              
-          morseCode = "";
-        }
-        return;
-      } else if (50 <= delta_time) {
-        Serial.println("Intra-character Space");
-        
-      }
-  } else {
-    Serial.println("[ERROR] Invalid function call.");
+void dataTransmitter(int mode, float delta_time) { // Sends data if code is C
+  char code = deltaTimeCalculator(mode, delta_time);
+  if (code == 'e' || code == NULL || code == ' ' || code == 0) return;
+  if (code == 'C') {
+    // Send morseCode, empty it
+    Serial.print("Sending the code now. \t\t");
+    Serial.println(morseCode);
+    bufferResetter();
+  } else if (code == '-' || code == '.' || code == 'U' || code == 'W') { // I didn't understand why there are some nully data passing on... also some duplicates, not sure if it's due to the accuracy of sensor
+    // Buffer morseCode
+    int encoded = customEncoder(code);
+    Serial.print("Code: \t\t");
+    Serial.println(code);
+    Serial.print("Encoded: \t");
+    Serial.println(encoded);
+    bufferConstructor(encoded);
   }
-  
+}
 
+void bufferResetter() {
+  morseCode = "";
+}
+
+void bufferConstructor(char code) {
+  morseCode += code;
 }
 
 
-void setup(){
-  
-  // pinMode(ledPin, OUTPUT);
-  // pinMode(buzzerPin, OUTPUT);
+int customEncoder(char code) {
+  for (int i = 0; i < sizeof(CUSTOM_ENCODING_TABLE) / sizeof(CUSTOM_ENCODING_TABLE[0]); i++) {
+    if (code == CUSTOM_ENCODING_TABLE[i]) return i + 1 + '0' ;
+  }
+}
+
+char deltaTimeCalculator(int mode, float delta_time) {
+  char output;
+  // Character
+  if (mode == 1) {
+    if ((timeunit * timingtable[1]) - padding <= delta_time) {
+      output = '-';
+    } else if (filter_limit <= delta_time) {
+      output = '.';
+    }
+  }
+  // Space
+  else if (mode == 0) {
+    if ((timeunit * timingtable[4]) - padding <= delta_time) {
+      output = 'W';
+    }
+    else if ((timeunit * timingtable[3]) - padding <= delta_time) {
+      output = 'C';
+    } else if (filter_limit <= delta_time) {
+      output = 'U';
+    }
+  } else {
+    Serial.println("[ERROR] Invalid mod number...");
+    return 'e'; // error
+  }
+  return output;
+}
+
+void setup() {
   pinMode(vibratorPin, OUTPUT);
   pinMode(activatorPin, OUTPUT);
-  digitalWrite(activatorPin, 1);
-  
   pinMode(sensorPin, INPUT);
-  
-  // morseToPulse(".-");
 
-  Serial.begin(9600);  
+  Serial.begin(9600);
   BTSerial.begin(38400);  // HC-05 default speed in AT command more
-  
+
   Serial.print("Timeunit: ");
-  Serial.println(timeunit);
+  Serial.println(timeunit / millisecond);
   Serial.print("Padding: ");
   Serial.println(padding);
-
 }
 
-void loop(){
-  
-  while(analogRead(sensorPin) < threshold);
-  
-  // tone(buzzerPin, 2000);
-  
-  initial_time = millis() * timespeed;
-  
-  delta_time = initial_time - final_time;
-  
-  // Serial.print("Low time ");
-  // Serial.println(delta_time); // t1 - t2
-  //Serial.println(delta_time / timespeed); // t1 - t2
-  
-  timeunitToMorse(0, delta_time);
-  // delay(padding);
-  
-  while(analogRead(sensorPin) > threshold);
-  
-  // noTone(buzzerPin);
-  
-  final_time = millis() * timespeed;
-  
-  delta_time = final_time - initial_time;
-  
-  // Serial.print("High time ");
-  // Serial.println(delta_time); // t2 - t1
-  //Serial.println(delta_time / timespeed); // t2 - t1
-  
-  timeunitToMorse(1, delta_time);
-  // delay(padding);
-  
-  // Serial.println(round(delta_time / timespeed / 60.0)); // Using speed because of the accuracy of Tinkercad's emulation timer.
+void loop() {
 
- 
-  
+  while (analogRead(sensorPin) < threshold);
+
+  if (!started) {
+    initial_time = final_time = millis();
+    Serial.println("Not started yet");
+  }
+  else final_time = millis();
+
+  final_time = millis();
+
+  digitalWrite(vibratorPin, 1);
+
+  delta_time = final_time - initial_time;
+
+  dataTransmitter(0, delta_time);
+
+  initial_time = final_time;
+
+  started = true;
+
+  while (analogRead(sensorPin) > threshold); // loop if no signal
+
+  final_time = millis();
+
+  digitalWrite(vibratorPin, 0);
+
+  delta_time = final_time - initial_time;
+
+  dataTransmitter(1, delta_time);
+
+  initial_time = final_time;
+
+
+
 }
